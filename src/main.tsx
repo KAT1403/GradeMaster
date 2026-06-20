37a451070df789b3a85c557bff6aa6e5b406c98f
Spectr123 <alex.kim.14032011@gmail.com>
2026-06-20 21:20:38 +0500

    Fix scroll problems

--- src/main.tsx
+++ src/main.tsx
@@ -4,20 +4,6 @@ import "./app/globals.scss";
 import "./shared/i18n/i18n";
 import App from "./app/App.tsx";
 
-document.addEventListener(
-  "touchstart",
-  (e) => {
-    if (e.touches.length > 1) {
-      e.preventDefault();
-    }
-  },
-  { passive: false }
-);
-
-document.addEventListener("gesturestart", (e) => {
-  e.preventDefault();
-});
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
